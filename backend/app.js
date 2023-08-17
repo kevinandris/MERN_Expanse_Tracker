71b6d8df72564eb0bc096c695112c736286ef664
keipy27 <kevinandris27@gmail.com>
2023-08-18 10:59:18 +1200

    transaction.js code implemented and added readdirSync from fs module to access the file inside route folder

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
+require ('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
-require ('dotenv').config()
+const { db } = require('./db/db')
+const { readdirSync } = require('fs')
 
 const PORT = process.env.PORT
 
-// middlewares
+// ! middlewares
 app.use(express.json())
 app.use(cors())
 
+// ! routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
+    db()
     app.listen(PORT, () => {
         console.log('listening to port: ', PORT)
     })
